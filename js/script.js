280c087802a81599640191920f365e3e59e9cad9
Md.Abu Tahir Sarker <msarker141151@bscse.uiu.ac.bd>
2022-02-17 21:34:11 +0600

    final update

--- js/script.js
+++ js/script.js
@@ -16,8 +16,7 @@ function expense() {
     else {
         alert('please input positive value');
         return 0;
-    }
-    
+    } 
 }
 
 
@@ -25,68 +24,71 @@ function balance() {
     // accessing Income input field
     const income = document.getElementById('income').value;
     const income_value = parseInt(income);
+
     if (income_value >= 0) {
         // getting value from expenses
         const expenses = parseInt(expense());
+        console.log(expenses);
         //setting value at expenses field
         if (expenses > income_value) {
             alert('Your expenses is higher than Income');
             return 0;
         }
-        else {
+        else if (expenses != 0) {
             document.getElementById('total-expenses').innerText = expenses;
             const amount = income_value - expenses;
             return amount;
         }
-    }
-    
-    
+    }  
 }
 
 //added event listener at balance button ara
 document.getElementById('calculate').addEventListener('click', function () {
     //setting value at balance field
     const balance_value = balance();
+
     if (!isNaN(balance_value)) {
         document.getElementById('balance').innerText = balance_value;
     }
     else {
         alert('Please fill the input fields with positive Numbers of amount');
     }
-})
+});
 
 //added event listener at save area
 document.getElementById('save-button').addEventListener('click', function () {
+    
     //getting value from save input field
     const save_input = document.getElementById('save-input').value;
     var save_input_value = parseInt(save_input);
     
     if (!isNaN(save_input_value) && save_input_value >= 0) {
+
         //income amount 
         const income = document.getElementById('income').value;
-        const income_value = parseInt(income);
+        let income_value = parseInt(income);
+
         //current balance
-        const currentBalance = balance();
+        let currentBalance = balance();
+        if (isNaN(income_value)) {
+            income_value = 0;
+            currentBalance = 0;
+        }
+        
         //Calculate saving amount
         const saving_amount = (income_value / 100) * save_input_value;
         
         if (saving_amount > currentBalance) {
             alert('Not sufficient balance for saving')
-        } else {
+        }
+        else {
             document.getElementById('saving-amount').innerText = saving_amount;
             //remaining balance 
             document.getElementById('remaining-balance').innerText = currentBalance - saving_amount;
-
-             document.getElementById('income').value = '';
-             document.getElementById('food').value = '';
-             document.getElementById('rent').value = '';
-             document.getElementById('clothes').value = '';
-             document.getElementById('save-input').value = '';
         }
+
     }
     else {
         alert('Please Input positive number value how many amount you want to save');
     }
 })
-
-//Finished project
