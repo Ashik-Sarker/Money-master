393c4a8332db19725154f2653ba5f0d901acbbe0
Md.Abu Tahir Sarker <msarker141151@bscse.uiu.ac.bd>
2022-02-17 11:08:57 +0600

    error handling

--- js/script.js
+++ js/script.js
@@ -9,7 +9,14 @@ function expense() {
     //getting value from clothes input field
     const clothes = document.getElementById('clothes').value;
     const clothes_cost = parseInt(clothes);
-    return (food_cost + rent_cost + clothes_cost);
+
+    if (food_cost >= 0 && rent_cost >= 0 && clothes_cost >= 0) {
+        return (food_cost + rent_cost + clothes_cost);
+    }
+    else {
+        alert('please input positive value');
+    }
+    
 }
 
 
@@ -17,33 +24,58 @@ function balance() {
     // accessing Income input field
     const income = document.getElementById('income').value;
     const income_value = parseInt(income);
-    // getting value from expenses
-    const expenses = parseInt(expense());
-    //setting value at expenses field
-    document.getElementById('total-expenses').innerText = expenses;
-    const amount = income_value - expenses;
-    return amount;
+    if (income_value >= 0) {
+        // getting value from expenses
+        const expenses = parseInt(expense());
+        //setting value at expenses field
+        document.getElementById('total-expenses').innerText = expenses;
+        const amount = income_value - expenses;
+        return amount;
+    }
+    
+    
 }
 
 //added event listener at balance button ara
 document.getElementById('calculate').addEventListener('click', function () {
     //setting value at balance field
-    document.getElementById('balance').innerText = balance();
+    const balance_value = balance();
+    if (!isNaN(balance_value)) {
+        document.getElementById('balance').innerText = balance_value;
+    }
+    else {
+        alert('Please fill the input fields with positive Numbers of amount');
+    }
 })
 
 //added event listener at save area
 document.getElementById('save-button').addEventListener('click', function () {
     //getting value from save input field
     const save_input = document.getElementById('save-input').value;
-    const save_input_value = parseInt(save_input);
-    //income amount 
-    const income = document.getElementById('income').value;
-    const income_value = parseInt(income);
-    //current balance
-    const currentBalance = balance();
-    //Calculate saving amount
-    const saving_amount = (income_value / 100) * save_input_value;
-    document.getElementById('saving-amount').innerText = saving_amount;
-    //remaining balance 
-    document.getElementById('remaining-balance').innerText = currentBalance - saving_amount;
+    var save_input_value = parseInt(save_input);
+    
+    if (!isNaN(save_input_value) && save_input_value >= 0) {
+        //income amount 
+        const income = document.getElementById('income').value;
+        const income_value = parseInt(income);
+        //current balance
+        const currentBalance = balance();
+        //Calculate saving amount
+        const saving_amount = (income_value / 100) * save_input_value;
+        document.getElementById('saving-amount').innerText = saving_amount;
+        if (saving_amount > currentBalance) {
+            alert('Not sufficient balance for saving')
+        } else{
+            //remaining balance 
+            document.getElementById('remaining-balance').innerText = currentBalance - saving_amount;
+        }
+        document.getElementById('income').value = '';
+        document.getElementById('food').value = '';
+        document.getElementById('rent').value = '';
+        document.getElementById('clothes').value = '';
+        document.getElementById('save-input').value = '';
+    }
+    else {
+        alert('Please Input positive number value how many amount you want to save');
+    }
 })
