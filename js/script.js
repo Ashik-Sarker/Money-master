289d754a06bd87eb1398d3e72de36e65669622e9
Md.Abu Tahir Sarker <msarker141151@bscse.uiu.ac.bd>
2022-02-17 21:41:38 +0600

    Project completed

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@
 function expense() {
     //getting value from food input field
     const food = document.getElementById('food').value;
-    const food_cost = parseInt(food);
+    const food_cost = parseFloat(food);
     //getting value from rent input field
     const rent = document.getElementById('rent').value;
-    const rent_cost = parseInt(rent);
+    const rent_cost = parseFloat(rent);
     //getting value from clothes input field
     const clothes = document.getElementById('clothes').value;
-    const clothes_cost = parseInt(clothes);
+    const clothes_cost = parseFloat(clothes);
 
     if (food_cost >= 0 && rent_cost >= 0 && clothes_cost >= 0) {
         return (food_cost + rent_cost + clothes_cost);
@@ -23,11 +23,11 @@ function expense() {
 function balance() {
     // accessing Income input field
     const income = document.getElementById('income').value;
-    const income_value = parseInt(income);
+    const income_value = parseFloat(income);
 
     if (income_value >= 0) {
         // getting value from expenses
-        const expenses = parseInt(expense());
+        const expenses = parseFloat(expense());
         console.log(expenses);
         //setting value at expenses field
         if (expenses > income_value) {
@@ -60,17 +60,18 @@ document.getElementById('save-button').addEventListener('click', function () {
     
     //getting value from save input field
     const save_input = document.getElementById('save-input').value;
-    var save_input_value = parseInt(save_input);
+    var save_input_value = parseFloat(save_input);
     
     if (!isNaN(save_input_value) && save_input_value >= 0) {
 
         //income amount 
         const income = document.getElementById('income').value;
-        let income_value = parseInt(income);
+        let income_value = parseFloat(income);
 
+        let expense_value = expense();
         //current balance
         let currentBalance = balance();
-        if (isNaN(income_value)) {
+        if (isNaN(income_value) || expense_value == 0) {
             income_value = 0;
             currentBalance = 0;
         }
